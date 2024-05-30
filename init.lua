5cf3c7327b38013c9dd4ca40523124507ac0f1a1
Måns Andersson <mansande@kth.se>
2024-05-30 14:13:10 +0200

    not sure if oil is the way to go

--- init.lua
+++ init.lua
@@ -7,13 +7,15 @@ vim.call('plug#begin')
 
 Plug('https://github.com/apple/pkl-neovim.git')
 Plug('nvim-treesitter/nvim-treesitter',{['do'] = function() vim.call(':TSUpdate') end})
+Plug('honza/vim-snippets')
 
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim')
 --                                    , {tag= '0.1.4' })
 --                                    , { 'branch': '0.1.x' }
 Plug('BurntSushi/ripgrep')
-
+Plug 'stevearc/oil.nvim'
+--Plug('stevearc/oiln.nvim')
 Plug 'junegunn/vim-easy-align'
 -- Hur fungerar easy align?" 
 
@@ -50,9 +52,27 @@ vim.call('plug#end')
 vim.g.coq_settings = { auto_start = "shut-up" }
 local coq = require("coq")
 vim.cmd([[COQnow]])
-
+vim.cmd([[set spell spelllang=en_gb]])
 require("options")
 require("keymaps")
+
+
+--" The below is required for enabling the tree-sitter syntax engine, which is used by pkl-neovim.
+--lua <<EOF
+local hasConfigs, configs = pcall(require, "nvim-treesitter.configs")
+if hasConfigs then
+  configs.setup {
+    ensure_installed = "pkl",
+    highlight = {
+      enable = true,              -- false will disable the whole extension
+    },
+    indent = {
+      enable = true
+    }
+  }
+end
+--EOF
+
 
 -- TO DO: 
 -- require("COQnow") 
